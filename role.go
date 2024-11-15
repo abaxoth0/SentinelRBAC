1ffb8ba1a88c948325af0be028edd3891679cf3d
Stepan Ananin <xrf848@gmail.com>
2024-11-15 15:50:09 +0300

    Added GetServiceRoles

--- role.go
+++ role.go
@@ -34,3 +34,21 @@ func ParseRole(roleName string, service *Service) (*Role, *Error) {
 
 	return nil, NewError("Роль \"" + roleName + "\" не надена")
 }
+
+func GetServiceRoles(serviceID string) ([]*Role, *Error) {
+	if !IsSchemaLoaded() {
+		return nil, NewError("RBAC is not loaded")
+	}
+
+	if serviceID == "" {
+		return nil, NewError("Missing service id")
+	}
+
+	for _, service := range Schema.Services {
+		if service.ID == serviceID {
+			return service.Roles, nil
+		}
+	}
+
+	return nil, NewError("service with id \"" + serviceID + "\" wasn't found")
+}
